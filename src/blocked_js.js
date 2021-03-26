077a1407c1b09e00b586607577390694c958a12f
pedro1090 <78980929+pedro1090@users.noreply.github.com>
2021-03-26 15:56:13 -0300

    Update blocked_js.js

--- src/blocked_js.js
+++ src/blocked_js.js
@@ -1,5 +1,20 @@
 //lista de coisas dentro de scripts que apontam anuncios
-var blocked_js = [
-    "adPlacements",
-    "playerAds",
-]
+file_in = open("block_araw.txt", "r")
+file_out = open("blocked_domains.js", "w")
+
+line = file_in.readline()
+file_out.write("var blocked_domains.js = [\n")
+
+first = True
+while(line):
+    if("127.0.0.1 " in line):
+        if not first:
+             file_out.write(",\n")
+        first = False
+        line = (line.lstrip("127.0.0.1 ")).rstrip("]\n")
+        parsed_line = "*://*." + line + "/*"
+        file_out.write("\"" + parsed_line + "\"")
+    line = file_in.readline()
+file_out.write("]")
+file_out.close()
+file_in.close()
