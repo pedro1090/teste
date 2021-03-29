a4c4e2a47e03b4dc5c443cd1f4ce5550534df5f1
pedro1090 <78980929+pedro1090@users.noreply.github.com>
2021-03-29 15:44:14 -0300

    Update blocked_js.js

--- src/blocked_js.js
+++ src/blocked_js.js
@@ -1,6 +1,6 @@
 //lista de coisas dentro de scripts que apontam anuncios
-file_in = open("block_araw.txt", "r")
-file_out = open("blocked_domains_v2.js", "w")
+file_in = open("domainsblockraw_v2.txt", "r")
+file_out = open("blockedsites_v2.js", "w")
 
 line = file_in.readline()
 file_out.write("var blocked_domains.js = [\n")
